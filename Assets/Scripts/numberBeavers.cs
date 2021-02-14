dcaa6f2f07fe51b404c8b066e0742bdb63198431
Ryam20151 <rbelange@uwaterloo.ca>
2021-02-13 22:03:48 -0500

    sprites for canal

--- Assets/Scripts/numberBeavers.cs
+++ Assets/Scripts/numberBeavers.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class numberBeavers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int turns;
+    public int totalTurns;
+
+    public Image[] beavers;
+    public Sprite fullBeaver;
+    public Sprite usedBeaver;
+
+    void Update(){
+        for(int i = 0; i<beavers.Length; i++){
+            if(i<turns){
+                beavers[i].sprite = fullBeaver;
+            } else {
+                beavers[i].sprite = usedBeaver;
+            }
+
+            if(i < totalTurns){
+                beavers[i].enabled = true;
+            } else {
+                beavers[i].enabled = false;
+            }
+        }
     }
 }
