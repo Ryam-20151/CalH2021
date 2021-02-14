9361d0bb23ca1083f73f77382e19b9cc02186c85
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2021-02-13 23:27:33 -0500

    typo fix

--- Assets/Scripts/ChannelNode.cs
+++ Assets/Scripts/ChannelNode.cs
@@ -34,7 +34,7 @@ public class ChannelNode : MonoBehaviour
     public Sprite rightCanalDamNoWater;
     public Sprite rightNoCanal;
 
-    public ChannelNode(HexCells[] tiles, int orientation, bool hasWater = false, bool isCanal = true, bool hasDam = false) {
+    public ChannelNode(HexCell[] tiles, int orientation, bool hasWater = false, bool isCanal = true, bool hasDam = false) {
         _tiles = tiles;
         _hasWater = hasWater;
         _isCanal = isCanal;
@@ -57,6 +57,17 @@ public class ChannelNode : MonoBehaviour
         }
 
         return true;
+    }
+
+    private void updateState(){
+        switch (_state){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
     }
 
     // Recursively iterate through all nodes to update the water
