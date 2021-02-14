f5aa784556371e852abbacd55e716809a10bb8d9
Code-Titan <artemis.sotnikov12@gmail.com>
2021-02-14 01:47:49 -0500

    Display Work

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -148,24 +148,50 @@ public class GameGrid : MonoBehaviour
                     if (curr_cell.nodes[0] == null)
                     {
                         HexCell[] p0list = { curr_cell, pos_0_cell };
-                        //ChannelNode pos_0_node = new ChannelNode(p0list, 0);
+                        
 
-                        GameObject pos_0_node = Instantiate(channelNodePrototype((p0list, -1)));
+                        float k = 1.4f;
+                        float d_x = (float)(j - 3);
+                        float d_y = (float)(i - 3);
+
+                        Vector3 initial_postion;
+                        initial_postion = new Vector3(k * d_x * (float)Math.Cos(Math.PI / 6), k * (d_y + 0.5f + Math.Abs(d_x) * (float)Math.Sin(Math.PI / 6)), 0f);
+
+                        GameObject pos_0_node = Instantiate(channelNodePrototype, initial_postion, Quaternion.identity);
+                        pos_0_node.GetComponent<ChannelNode>().configureNode(p0list, 0);
                         curr_cell.nodes[0] = pos_0_node;
+                        tiles[i + 1, j].GetComponent<HexCell>().nodes[0] = pos_0_node;
                     }
 
                     // Position 1
                     if (curr_cell.nodes[1] == null)
                     {
                         HexCell[] p1list = { curr_cell, pos_1_cell };
-                        ChannelNode pos_1_node = new ChannelNode(p1list, -1);
+
+                        float k = 1.4f;
+                        float d_x = (float)(j - 3);
+                        float d_y = (float)(i - 3);
+
+                        Vector3 initial_postion;
+                        initial_postion = new Vector3(k * (d_x + 0.5f)* (float)Math.Cos(Math.PI / 6), k * (d_y + 0.25f + Math.Abs((d_x)) * (float)Math.Sin(Math.PI / 6)), 0f);
+
+                        GameObject pos_1_node = Instantiate(channelNodePrototype, initial_postion, Quaternion.identity);
+                        pos_1_node.GetComponent<ChannelNode>().configureNode(p1list, -1);
                         curr_cell.nodes[1] = pos_1_node;
                     }
                     // Position 2
                     if (curr_cell.nodes[2] == null)
                     {
                         HexCell[] p2list = { curr_cell, pos_2_cell };
-                        ChannelNode pos_2_node = new ChannelNode(p2list, 1);
+                        float k = 1.4f;
+                        float d_x = (float)(j - 3);
+                        float d_y = (float)(i - 3);
+
+                        Vector3 initial_postion;
+                        initial_postion = new Vector3(k * (d_x + 0.5f) * (float)Math.Cos(Math.PI / 6), k * (d_y - 0.25f + Math.Abs((d_x)) * (float)Math.Sin(Math.PI / 6)), 0f);
+
+                        GameObject pos_2_node = Instantiate(channelNodePrototype, initial_postion, Quaternion.identity);
+                        pos_2_node.GetComponent<ChannelNode>().configureNode(p2list, 1);
                         curr_cell.nodes[2] = pos_2_node;
                     }
 
@@ -173,21 +199,51 @@ public class GameGrid : MonoBehaviour
                     if (curr_cell.nodes[3] == null)
                     {
                         HexCell[] p3list = { curr_cell, pos_3_cell };
-                        ChannelNode pos_3_node = new ChannelNode(p3list, -1);
+
+                        float k = 1.4f;
+                        float d_x = (float)(j - 3);
+                        float d_y = (float)(i - 3);
+
+                        Vector3 initial_postion;
+                        initial_postion = new Vector3(k * d_x * (float)Math.Cos(Math.PI / 6), k * (d_y - 0.5f + Math.Abs(d_x) * (float)Math.Sin(Math.PI / 6)), 0f);
+
+                        GameObject pos_3_node = Instantiate(channelNodePrototype, initial_postion, Quaternion.identity);
+                        pos_3_node.GetComponent<ChannelNode>().configureNode(p3list, 0);
                         curr_cell.nodes[3] = pos_3_node;
+                        tiles[i + 1, j].GetComponent<HexCell>().nodes[3] = pos_3_node;
                     }
                     // Position 4
                     if (curr_cell.nodes[4] == null)
                     {
                         HexCell[] p4list = { curr_cell, pos_4_cell };
-                        ChannelNode pos_4_node = new ChannelNode(p4list, 1);
+                        float k = 1.4f;
+                        float d_x = (float)(j - 3);
+                        float d_y = (float)(i - 3);
+
+                        Vector3 initial_postion;
+                        initial_postion = new Vector3(k * (d_x - 0.5f) * (float)Math.Cos(Math.PI / 6), k * (d_y - 0.25f + Math.Abs((d_x)) * (float)Math.Sin(Math.PI / 6)), 0f);
+
+                        GameObject pos_4_node = Instantiate(channelNodePrototype, initial_postion, Quaternion.identity);
+                        pos_4_node.GetComponent<ChannelNode>().configureNode(p4list, -1); 
+
                         curr_cell.nodes[4] = pos_4_node;
                     }
                     // Position 5
                     if (curr_cell.nodes[5] == null)
                     {
                         HexCell[] p5list = { curr_cell, pos_5_cell };
-                        ChannelNode pos_5_node = new ChannelNode(p5list, 0);
+                        HexCell[] p1list = { curr_cell, pos_1_cell };
+
+                        float k = 1.4f;
+                        float d_x = (float)(j - 3);
+                        float d_y = (float)(i - 3);
+
+                        Vector3 initial_postion;
+                        initial_postion = new Vector3(k * (d_x - 0.5f) * (float)Math.Cos(Math.PI / 6), k * (d_y + 0.25f + Math.Abs((d_x)) * (float)Math.Sin(Math.PI / 6)), 0f);
+
+                        GameObject pos_5_node = Instantiate(channelNodePrototype, initial_postion, Quaternion.identity);
+                        pos_5_node.GetComponent<ChannelNode>().configureNode(p5list, 1);
+                        
                         curr_cell.nodes[5] = pos_5_node;
                     }
                 }
