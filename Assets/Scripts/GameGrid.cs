6279676ef60fbfd34326c34c8142b08f6318dbac
Code-Titan <artemis.sotnikov12@gmail.com>
2021-02-13 19:15:46 -0500

    Starting work with hex cells.

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+
+    public HexTile hexTilePrototype;
+    public HexTile[7][7] tiles = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (i == 4 && (j < 1 || j > 5))
+                {
+                    tiles[i][j] = null;
+                }
+                else if (i == 5 && (j < 2 || j > 4))
+                {
+                    tiles[i][j] = null;
+                }
+                else if (i == 6 && j != 4)
+                {
+                    tiles[i][j] = null;
+                }
+                else
+                {
+                    tiles[i][j] = Instantiate(hexTilePrototype);
+                }
+            }
+        }    
+        
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
